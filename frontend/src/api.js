f0f4d1af5fc389d6acff19ecfd9092d2ee3368b4
IT24101938 <it2410193@my.sliit.lk>
2026-04-28 20:25:50 +0530

    update API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://dep-test-dge3.onrender.com",
+  baseURL: import.meta.env.VITE_API_URL || "https://dep-test-dge3.onrender.com/api",
 });
 
 export const getItems = () => API.get('/items');
